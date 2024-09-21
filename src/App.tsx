45484ae1df980d2f1a6e91ed5e60e08a0f51adba
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-09-20 22:01:26 -0300

    Criado TaskForm e estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
+//css
+import styles from './App.module.css'
+
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
-//css
-import styles from './App.module.css'
 
 function App() {
   return (
     <div>
       <Header />
       <main className={styles.main}>
-        <h1>Conteudo</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar tarefa"/>
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </div>
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TaskList = (props: Props) => {
+  return (
+    <div><p>Lista Tarefas</p></div>
+  )
+}
+
+export default TaskList
