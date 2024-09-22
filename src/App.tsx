eddca0f5b666b260828a8d95db3f281d9c1e124d
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-09-22 16:02:38 -0300

    Criado componente Modal

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Modal from './components/Modal';
 
 // interface
 import { ITask } from './interfaces/Task';
-
 
 function App() {
 
@@ -27,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <Modal children={ <TaskForm btnText='Editar Tarefa' taskList={taskList}/> }/>
       <Header />
       <main className={styles.main}>
         <div>
